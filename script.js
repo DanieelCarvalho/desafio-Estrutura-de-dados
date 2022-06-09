285fb16d66699f797885fb71f74bda546d8171e1
DanieelCarvalho <Danielscg2012@gmail.com>
2022-06-09 19:37:16 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let nome = "Daniel Carvalho";
+let ano = 1993;
+let anoAtual = 2022;
+let resultado= anoAtual - ano;
+console.log(resultado);
+console.log("Estou mexendo no console.log")
+let quartaFeira= false;
+console.log(quartaFeira);
